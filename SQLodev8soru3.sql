fae631bf018ab612f2b916c13c59aae16a3f7f81
izzetcosar <izzetcosar@gmail.com>
2024-11-21 15:23:27 +0300

    Add files via upload

---
+++ SQLodev8soru3.sql
@@ -0,0 +1,19 @@
+UPDATE employee
+SET name = 'Izzet COSAR'
+WHERE id = 1;
+
+UPDATE employee
+SET email = 'izzetcosar@gmail.com'
+WHERE name = 'Izzet COSAR';
+
+UPDATE employee
+SET birthday = '12.10.1994'
+WHERE email = 'izzetcosar@gmail.com';
+
+UPDATE employee
+SET name = 'Hamit CAN POLAT'
+WHERE id = 2;
+
+UPDATE employee
+SET email = 'Hamitcanpolat@gmail.com'
+WHERE name = 'Hamit CAN POLAT';

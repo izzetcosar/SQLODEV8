0d708defb2da2581ea7a01d8997be0979d9a0ff8
izzetcosar <izzetcosar@gmail.com>
2024-11-21 15:28:12 +0300

    Add files via upload

---
+++ SQLodev8soru4.sql
@@ -0,0 +1,14 @@
+DELETE FROM employee
+WHERE id = 3;
+
+DELETE FROM employee
+WHERE name = 'Belva Mearns';
+
+DELETE FROM employee
+WHERE birthday = '1930-09-17';
+
+DELETE FROM employee
+WHERE email = 'twhymark5@cafepress.com';
+
+DELETE FROM employee
+WHERE id = 7;
